7b8c396e0deec897cc41a92a2433e97f5806454b
Torsten Becker <torsten.becker@gmail.com>
2013-05-13 10:27:09 -0600

    Adding my cheats from the last couple months.

--- cheat.rb
+++ cheat.rb
@@ -66,6 +66,27 @@ end
 
 # Add more cheats here:
 __END__
+Update port list and MacPorts itself:
+
+  sudo port selfupdate
+
+List outdated ports:
+
+  port outdated
+    
+List installed ports
+
+  port installed
+
+Clean ports
+
+  sudo port uninstall inactive
+  sudo port clean --all installed
+
+Upgrade/update existing port without dependency update
+
+  sudo port upgrade -n git-core
+
 Changes MAC address:
 
   sudo ifconfig en1 ether 00:11:22:33:44:55
@@ -74,16 +95,22 @@ Delete SVN directories:
 
   find . -name .svn -print0 | xargs -0 rm -rf
 
-Showing extended file attributes:
+Showing extended file attributes and recover source URL of web downloads:
 
   ls -@ video.m4v 
-  -rw-r--r--@ 1 torsten   staff   2.7M Nov 17 00:34 video.m4v
-  	com.apple.metadata:kMDItemWhereFroms	 157B 
-  xattr -p com.apple.metadata:kMDItemWhereFroms video.m4v 
+    -rw-r--r--@ 1 root    staff   2.7M Nov 17 00:34 video.m4v
+    	com.apple.metadata:kMDItemWhereFroms	 157B 
+  xattr -p com.apple.metadata:kMDItemWhereFroms video.m4v | \
+  ruby -ne 'print [$_.gsub /\s/,""].pack("H*")'
+      bplist00?_@http://cloud.github.com/downloads/...
 
 Saving as root from within an unprivileged vim:
 
   :w !sudo tee % > /dev/null
+
+Finding files:
+
+  find /pathname -iname '*.md'
 
 Flush DNS Cache:
 
@@ -108,3 +135,112 @@ Generating the raw data for a git impact chart ala GitHub:
 Remove *.pyc files:
 
   find . -iname '*.pyc' | xargs -n1 rm
+
+Print bit-size and fingerprint of all local SSH public keys:
+
+  for i in ~/.ssh/*.pub; do ssh-keygen -l -f "$i"; done
+
+Search zsh history:
+
+  !ssh            # Starts with "ssh"
+  !?ivy           # Contains "ivy" anywhere
+  !!              # Last typed command
+  <Ctrl-R>word    # Incrementally start searching in history
+
+Search for assets and copy them to somewhere else:
+
+  find . -iname 'WKRounded*.png' -print0 | xargs -0 -J% -n1 cp % ~/tmp
+
+  # -print0 and -0 allow spaces in filenames
+  # -J% sets % as a replacement character for the command.
+
+Recursively searching for file contents using grep:
+
+  # pattern file/folder
+  grep -R WKSpringboardIcon512 .
+
+Do something for each file in a loop:
+
+  for FILE in *.rb; do; wc -c $FILE; done
+
+Undo last git commit:
+
+  git reset --soft HEAD^
+
+Stage only some selected chunks of a diff for committing:
+
+  git add -p filename.x
+
+List available Wi-Fi/WLAN networks and their channels:
+
+  /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s
+
+Remove/delete a remote branch:
+
+  git push origin :torsten/some-branch-name
+
+Redo/revert/undo a Git commit:
+  
+  git commit --amend
+
+Redirect/pipe stderr:
+
+  git status > /dev/null 2>&1
+
+Fork a public repo as a private one
+ 
+  # https://help.github.com/articles/duplicating-a-repo
+  git clone --bare https://github.com/exampleuser/REPO.git
+  cd old-repo.git
+  git push --mirror git@github.com:6wunderkinder/REPO.git
+
+Find out which process is listening to DNS port 53:
+
+  sudo lsof -n -i4TCP:53
+
+Shell infinite loop:
+
+  while true; do echo 'some something FOREVER'; sleep 1; done
+
+Generate release notes from git history:
+
+  git log --no-merges --pretty=format:"  * %s (%an)" hockeyapp...master
+
+In vim: When searching via / clear highlight:
+
+  :noh
+
+In vim: Commenting code:
+
+  " Commenting using nerdcommenter
+  Visual mode (v); mark text; <Leader>cc
+  " Uncommenting:
+  v; mark text; <Leader>cu
+
+In vim: Block editing:
+ 
+  " CTRL-V: Visual block mode
+  Move to mark lines (jjj...); jump to end of line ($); Append (A); insert text; ESC
+  " To inset in the beginning:
+  Visual block; mark lines; Insert in beginning (I); insert text; ESC
+
+Show all files that changed in the last week:
+
+  git log --pretty=format: --name-only --since="7 days ago" | sort -u
+
+Pattern rename all files in a directory:
+
+  # Renames all files from .jpeg to $.jpg
+  find . -name "*.jpeg" | while read file; do mv $file .`echo $file|cut -d. -f2`.jpg; done
+
+Switch between the last two open files in vim:
+
+  Ctrl-^
+
+Change date of previous commit:
+
+  # There are author and committer date, this changes both:
+  export GIT_COMMITTER_DATE="Fri Apr 26 19:40:11 2013 -0600"
+  git commit --amend --date="Fri Apr 26 19:40:11 2013 -0600"
+
+
